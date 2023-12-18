30fe1e1a313429019089637290179073a07ed0e4
Felipe Gebara Moreira <feagmoreira@hotmail.com>
2023-12-17 22:04:30 -0500

    Initial task 3

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Budget from './components/Budget';
 // Add code to import the other components here under
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseItem from './components/ExpenseItem';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
+import Currency from './components/Currency';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -27,6 +27,9 @@ const App = () => {
                 </div>
                 <div className='col-sm'>
                     <ExpenseTotal />
+                </div>
+                <div className='col-sm'>
+                    <Currency />
                 </div>
             </div>
             <h3 className='mt-3'>Allocation</h3>
